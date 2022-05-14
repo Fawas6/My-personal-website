8e5e7cd41d9f3c236612b3bcd32fce2487923fd4
Fawas6 <87198474+Fawas6@users.noreply.github.com>
2022-05-14 13:08:07 +0100

    Add files via upload

--- JS/formvalidator.js
+++ JS/formvalidator.js
@@ -1,7 +1,42 @@
 //Form Validation
 
-export default class formvalidator {
+export default class FormValidator {
     constructor(selector) {
         this.form = document.querySelector(selector);
+        this.inputsWithErrors = new Set();
+
+        this.form.addEventListener("submit", e => {
+            e.preventDefault();
+
+            if (!this.hasErrors) {
+                this.form.submit();
+            }
+        });
+    }
+
+    get hasErrors() {
+        return this.inputsWithErrors.size > 0;
+    }
+
+    register(selector, check) {
+        const inputField = this.form.querySelector(selector);
+        const errorElement = inputField.closest(".input").querySelector("#error_message");
+
+        const execute = (hideErrors) => {
+            const { pass, error } = check(inputField.value, inputField);
+
+            if (!hideErrors) {
+                errorElement.textContent = error || "";
+            }
+
+            if (!pass) {
+                this.inputsWithErrors.add(inputField);
+            } else {
+                this.inputsWithErrors.delete(inputField);
+            }
+        };
+
+        inputField.addEventListener("change", () => execute());
+        execute(true);
     }
 }
--- JS/main.js
+++ JS/main.js
@@ -2,6 +2,21 @@
 
 import formvalidator from "./formvalidator.js";
 
-const fv = new formvalidator("#reachout");
+const fv = new formvalidator(".row");
+
+function validatelength(value, inputField) {
+    if (value.length === 0 || value.length > 5) {
+        return {
+            pass: false,
+            error: "Username must be between 1 - 5 characters."
+        };
+    }
+
+    return {
+        pass: true
+    };
+}
+
+fv.register("#txt", validatelength);
 
 window.fv = fv;
